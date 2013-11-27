a185f51767c3d282805a41ab78dbb1ebe81566e5
hxtpoe <hxtpoe@gmail.com>
2013-11-27 22:55:28 +0100

    fix setup...

--- setup.sh
+++ setup.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 cp /var/www/html/jmeter/app/config/parameters.yml.dist /var/www/html/jmeter/app/config/parameters.yml
 COMPOSER_HOME="/var/www/html/jmeter/" composer install
-mkdir app/cache
-mkdir app/logs
 chmod -R 777 app/cache app/logs
 php app/console cache:clear --env=prod --no-warmup
