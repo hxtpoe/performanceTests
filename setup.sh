d55a55bee045d99a7b704c24fc5c1a460163bb0f
hxtpoe <hxtpoe@gmail.com>
2014-02-01 23:21:59 +0100

    fix missing mongo db in setup

--- setup.sh
+++ setup.sh
@@ -2,5 +2,6 @@
 cp /var/www/html/jmeter/app/config/parameters.yml.dist /var/www/html/jmeter/app/config/parameters.yml
 COMPOSER_HOME="/var/www/html/jmeter/" composer install
 chmod -R 777 app/cache app/logs
+mongo --eval "db.getSiblingDB('jmeter').addUser('travis', 'test');"
 php app/console cache:clear --env=prod --no-warmup
 php app/console doctrine:mongodb:fixtures:load
